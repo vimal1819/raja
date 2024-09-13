d1e9f467236995e80c8ba792d36b94cb1fe0102d
vimal1819 <vimalamrish@gmail.com>
2024-09-13 12:45:36 +0530

    third push

--- src/test/java/org/raja/Rajalogin.java
+++ src/test/java/org/raja/Rajalogin.java
@@ -10,5 +10,14 @@ public class Rajalogin {
 	public void method3() {
 		System.out.println("method3");
 	}
+	public void method7() {
+		System.out.println("method7");
+	}
+	public void method8() {
+		System.out.println("method8");
+	}
+	public void method9() {
+		System.out.println("method9");
 
+}
 }

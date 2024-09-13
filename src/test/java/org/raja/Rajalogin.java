7f9799fde4f9f1bcdb18fc0de5693c0cc40bdfda
vimal1819 <vimalamrish@gmail.com>
2024-09-13 12:27:58 +0530

    first push

---
+++ src/test/java/org/raja/Rajalogin.java
@@ -0,0 +1,14 @@
+package org.raja;
+
+public class Rajalogin {
+	public void method1() {
+		System.out.println("method");
+	}
+	public void method2() {
+		System.out.println("method2");
+	}
+	public void method3() {
+		System.out.println("method3");
+	}
+
+}
